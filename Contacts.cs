56f9ba372e7f9136aa4b7c5311d9028273642acb
voxchristy <christy.thomas@live.com>
2023-03-08 13:02:08 +0530

    Added Domain Validations

--- Contacts.cs
+++ Contacts.cs
@@ -1,14 +1,37 @@
-﻿namespace ContactManager.API
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactManager.API
 {
     public class Contacts
     {
         public int Id { get; set; }
+
+
+        [Required]
+        [MinLength(2, ErrorMessage ="Salutation length should be minimum of 2 characters")]
+        [MaxLength(50)]
         public string Salutation { get; set; }
+
+        [Required]
+        [MinLength(2, ErrorMessage = "FirstName length should be minimum of 2 characters")]
+        [MaxLength(50)]
         public string FirstName { get; set; }
+
+
+        [Required]
+        [MinLength(2, ErrorMessage = "LastName length should be minimum of 2 characters")]
+        [MaxLength(50)]
         public string LastName { get; set; }
+
+        [MaxLength(150)]
         public string DisplayName { get; set; }
         public DateTime BirthDate { get; set; }
+
+
+        [EmailAddress]
         public string Email { get; set; }
-        public string PhoneNumber { get; set; }
+        
+        [Phone]
+        public string? PhoneNumber { get; set; }
     }
 }
